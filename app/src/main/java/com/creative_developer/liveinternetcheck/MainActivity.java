6e3d0b7698b46cb84228b8582b6f7b4f5d531239
Rayhan Uddin Rafi <145839691+rafi-20189@users.noreply.github.com>
2023-09-23 20:14:48 +0600

    Updated

--- app/src/main/java/com/creative_developer/liveinternetcheck/MainActivity.java
+++ app/src/main/java/com/creative_developer/liveinternetcheck/MainActivity.java
@@ -2,13 +2,92 @@ package com.creative_developer.liveinternetcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.IntentFilter;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NetworkChangeReceiver.NetworkChangeListener{
+
+    NetworkChangeReceiver networkChangeReceiver;
+    AlertDialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        networkChangeReceiver = new NetworkChangeReceiver(this);
+
     }
+
+
+
+    //------------------------- Check internet ------------------
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(networkChangeReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    @Override
+    public void onNetworkChanged(boolean isConnected) {
+        if (isConnected) {
+
+            if (dialog != null && dialog.isShowing()) {
+                dialog.dismiss();
+                dialog = null;
+            }
+
+        } else {
+
+            if (dialog == null || !dialog.isShowing()) {
+
+                ShowDialog();
+            }
+
+        }
+    }
+
+
+    //--------------------------------- Dialog Show --------------------------------
+    private void ShowDialog() {
+
+        dialog = new AlertDialog.Builder(MainActivity.this)
+                .setView(R.layout.no_internet_dialog)
+                .setCancelable(false)
+                .create();
+        dialog.show();
+
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.R.color.transparent));
+
+
+        TextView playButton = dialog.findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(view->{
+
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+
+
+
 }
